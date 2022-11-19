6220617f8b046e5d3d6458bae93724fa1f7ec9c0
ejiofor <ejioforched@gmail.com>
2022-11-19 14:43:34 +0300

    space complexity

--- Algorithms/bigO.js
+++ Algorithms/bigO.js
@@ -23,10 +23,20 @@ function summation(n){
 // *** POINT 1 ****
 // summation (4) = 10 ---
 // 1 + 2 + 3 + 4 = 10
-The time complexity entails claculating the number of times a statement executes based on input size.
+// The time complexity entails claculating the number of times a statement executes based on input size. signified as 'n+2'
+
+// n + 2 , 4 + 2, 
+// big -o is expressed in terms of the input, where n is the input
 
 
-// big -o is expressed in terms of the input
+// *** POINT 2 BIGGER PICTURE ...****
+// -- Linear Time complexity = O(n) as the size of the input increases the time complexity increases.
+// --- Whenever you see a loop, the time complexity is linear
 
+function summation(n){
+	return (n * (n + 1)) / 2;
+}
 
-// *** POINT 2 ****
+// SPACE COMPLEXITY
+
+// 0(1)
