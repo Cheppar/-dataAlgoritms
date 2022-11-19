c4045c75f0ca496aeede6fb593acf23194e7f70c
ejiofor <ejioforched@gmail.com>
2022-11-19 20:12:35 +0300

    Math alogrithms intro.

---
+++ Algorithms/mathAlgorithms.js
@@ -0,0 +1,18 @@
+//MATH ALGORITHMS
+// - includes basic math algorithms
+We will cover
+1. Fibonacci sequence
+2. Factorial of a number
+3. Prime number
+4. Power of two
+5. Recursion
+
+// - IMPLEMENT
+1. Fibonacci sequence with recursion
+2. Factorial of a number with recursion
+
+// STEPS: 
+1. Problem statement
+2. Pause the video and give it a try
+3. Implement the solution together.
+4. Determione the big O time complexity of the solution.
